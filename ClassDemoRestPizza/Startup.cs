dad1e38dd4845de63e61b7610c34ce34b300aa1c
Philip Lind Jensen <jogjensen@hotmail.com>
2020-09-21 12:47:39 +0200

    Added swagger

--- ClassDemoRestPizza/Startup.cs
+++ ClassDemoRestPizza/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace ClassDemoRestPizza
 {
@@ -25,6 +26,11 @@ namespace ClassDemoRestPizza
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSwaggerGen(c =>
+                {
+                    c.SwaggerDoc("v1", new OpenApiInfo {Title="Items API",Version = "v1.0"});
+                }
+            );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,6 +49,12 @@ namespace ClassDemoRestPizza
             {
                 endpoints.MapControllers();
             });
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Items API v1.0")
+            );
         }
     }
 }
